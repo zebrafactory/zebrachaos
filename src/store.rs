ddc4048e415101a8b880b93f12a9323d2fc21e87
Jason Gerard DeRose <jasonderose@gmail.com>
2026-09-15 18:19:04 -0600

    Add workaround for Window's lack of pread64()

--- src/store.rs
+++ src/store.rs
@@ -1,7 +1,12 @@
 use crate::{HEADER, Hash, Index, Item, Object, ObjectHeader};
 use std::fs::File;
 use std::io::{self, Read, Seek, Write};
+
+#[cfg(not(windows))]
 use std::os::unix::fs::FileExt;
+
+#[cfg(windows)]
+use std::os::windows::fs::FileExt;
 
 pub struct Store {
     file: File,
@@ -21,17 +26,50 @@ impl Store {
         }
     }
 
-    pub fn read_entry(&self, item: &Item, buf: &mut Vec<u8>) -> io::Result<()> {
-        buf.resize(item.header.size(), 0);
-        self.file.read_exact_at(buf, item.offset)?;
-        Ok(())
+    #[cfg(not(windows))]
+    #[inline]
+    fn read_exact_at(&self, buf: &mut [u8], offset: u64) -> io::Result<()> {
+        self.file.read_exact_at(buf, offset)
+    }
+
+    #[cfg(windows)]
+    fn read_exact_at(&self, buf: &mut [u8], offset: u64) -> io::Result<()> {
+        // FIXME: There should totally be a seek_read_exact() method for Winders.
+        let mut buf = buf;
+        let mut offset = offset;
+        while !buf.is_empty() {
+            match self.file.seek_read(buf, offset) {
+                Ok(0) => {
+                    break;
+                }
+                Ok(n) => {
+                    buf = &mut buf[n..];
+                    offset += n as u64;
+                }
+                Err(ref e) if e.kind() == io::ErrorKind::Interrupted => {}
+                Err(e) => {
+                    return Err(e);
+                }
+            }
+        }
+        if !buf.is_empty() {
+            Err(io::Error::new(
+                io::ErrorKind::UnexpectedEof,
+                "failed to read object header plus object data",
+            ))
+        } else {
+            Ok(())
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use tempfile;
 
     #[test]
-    fn test_store() {}
+    fn test_store() {
+        let mut file = tempfile::tempfile().unwrap();
+    }
 }
