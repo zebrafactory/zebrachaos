e756bc0fc7fa7d86f6fa95d59c8a8a8d78c958d9
Jason Gerard DeRose <jasonderose@gmail.com>
2026-09-21 15:45:52 -0600

    Oops, cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,7 @@
 #![cfg_attr(all(windows, nightly), feature(seek_read_exact_seek_write_all))]
-
 #![forbid(unsafe_code)]
 #![warn(missing_docs)]
 #![deny(rustdoc::broken_intra_doc_links)]
-
 
 //! 🦓 🤪 ZebraChaos: A Git-like Content Hash Addressable Object Store.
 //!
